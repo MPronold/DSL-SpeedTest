f1e65f4b0306c0ab0241bc31aec150290ac82a12
Michael Pronold <michaelpronold@MBP-von-Michael.fritz.box>
2021-01-03 14:11:03 +0100

    initial upload

---
+++ SpeedTest.py
@@ -0,0 +1,11 @@
+''' Short-Code zu check your internet-speed. 
+If you have some problems check out following stackoverflow-post: 
+https://stackoverflow.com/questions/56326644/python-speedtest-facing-problems-with-certification-ssl-c1056'''
+
+import speedtest
+test = speedtest.Speedtest()
+down = test.download()
+upload = test.upload()
+
+print(f'Download speed: {down}')
+print(f'Uplaod speed: {upload}')
